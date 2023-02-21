33e819128c19d90952d619703333b7933080c206
Ify Okafor <ifeomasandra13@gmail.com>
2023-02-21 12:13:45 +0100

    Updated website

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const navList = document.getElementById("navBar");
+/*const navList = document.getElementById("navBar");
 
 window.onscroll = function(){
   if(document.body.scrollTop < 0 || document.documentElement.scrollTop > 5){
@@ -14,4 +14,21 @@ function displayFunction() {
   const p = document.getElementById('search-bar');
 
   p.innerHTML = `<input class="form-control me-2 search" type="search" placeholder="Search for artist, event or venue" aria-label="Search">`;
+}*/
+
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("navBar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("headTransparent");
+  } else {
+    header.classList.remove("headTransparent");
+  }
 }
